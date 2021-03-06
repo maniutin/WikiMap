e51a602dc1457f56f6057b9b9992073bac2aa0c9
Nick Maniutin <maniutin@gmail.com>
2021-03-06 20:59:57 +0000

    setup initial db schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,4 +1,3 @@
--- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS maps CASCADE;
@@ -9,7 +8,7 @@ DROP TABLE IF EXISTS point_favourites CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+ ; name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   thumbnail_url VARCHAR(255) NOT NULL
