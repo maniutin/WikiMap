08917bb855f93747ca084965013fbfad9dc3b8da
Nick Maniutin <maniutin@gmail.com>
2021-03-08 22:32:02 +0000

    register form adds user to db

--- routes/register.js
+++ routes/register.js
@@ -6,61 +6,77 @@ const router = express.Router();
 const { findUser } = require("../helpers");
 // const { response } = require("express");
 
+// module.exports = (db) => {
+//   router.get("/", (req, res) => res.render("register"));
+
+//   router.post("/", (req, res) => {
+//     db.query(`SELECT * FROM users;`)
+//       .then((data) => {
+//         const users = data.rows;
+
+//         const getUser = findUser(req.body.email, users);
+//         const password = req.body.password;
+//         req.body.canRegister = false;
+
+//         if (!getUser) {
+//           req.body.canRegister = true;
+//         }
+
+//         return req.body;
+//       })
+//       .then((bodyObject) => {
+//         const canRegister = bodyObject.canRegister;
+//         const emailStr = bodyObject.email;
+//         const password = req.body.password;
+//         // const hashedPassword = bcrypt.hashSync(bodyObject.password, 10);
+
+//         if (canRegister) {
+//           const queryParams = [
+//             bodyObject.username,
+//             emailStr,
+//             password,
+//             bodyObject.thumbnail,
+//           ];
+//           db.query(
+//             `
+//             INSERT INTO users (name, email, password, thumbnail_url) VALUES ($1, $2, $3, $4) RETURNING *;
+//             `,
+//             queryParams
+//           ).then((response) => {
+//             const user = response.rows[0];
+//             console.log(user);
+//             // req.session.user_id = user.id;
+//             const data = {
+//               error: false,
+//             };
+//             res.json(data);
+//           });
+//         } else {
+//           const data = {
+//             error: true,
+//           };
+//           res.json(data);
+//         }
+//       })
+//       .catch((err) => {
+//         res.status(500).json({ error: err.message });
+//       });
+//   }); // router post
+//   return router;
+// };
+
 module.exports = (db) => {
   router.get("/", (req, res) => res.render("register"));
 
   router.post("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then((data) => {
-        const users = data.rows;
-
-        const getUser = findUser(req.body.email, users);
-        const password = req.body.password;
-        req.body.canRegister = false;
-
-        if (!getUsere) {
-          req.body.canRegister = true;
-        }
-
-        return req.body;
-      })
-      .then((bodyObject) => {
-        const canRegister = bodyObject.canRegister;
-        const emailStr = bodyObject.email;
-        const password = req.body.password;
-        // const hashedPassword = bcrypt.hashSync(bodyObject.password, 10);
-
-        if (canRegister) {
-          const queryParams = [
-            bodyObject.username,
-            emailStr,
-            password,
-            bodyObject.thumbnail,
-          ];
-          db.query(
-            `
-            INSERT INTO users (username, email, password, thumbnail_url) VALUES ($1, $2, $3, $4) RETURNING *;
-            `,
-            queryParams
-          ).then((response) => {
-            const user = response.rows[0];
-            console.log(user);
-            // req.session.user_id = user.id;
-            const data = {
-              error: false,
-            };
-            res.json(data);
-          });
-        } else {
-          const data = {
-            error: true,
-          };
-          res.json(data);
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    console.log(req.body);
+    db.query(
+      `
+      INSERT INTO users (name, email, password, thumbnail_url) VALUES ('${req.body.name}', '${req.body.email}', '${req.body.password}', 'http://happyfacesparty.com/wp-content/uploads/2019/06/avataaars-Frances.png') RETURNING *;
+      `
+    )
+      .then((res) => res.rows)
+      .catch((err) => err);
   }); // router post
   return router;
 };
