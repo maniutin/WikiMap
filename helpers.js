e7a5cdc8dd663520dd9f86df3dffd90bb74d4905
Nick Maniutin <maniutin@gmail.com>
2021-03-09 02:29:07 +0000

    create login that retains the cookie

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const findUserByEmail = (emailID, users) => {
   for (const userID in users) {
     const userEmail = users[userID].email;
@@ -8,6 +10,19 @@ const findUserByEmail = (emailID, users) => {
   return false;
 };
 
+const checkPassword = (userObj, email, passwordToCheck) => {
+  const userEmail = userObj.email;
+
+  if (
+    userEmail === email &&
+    bcrypt.compareSync(passwordToCheck, userObj.password)
+  ) {
+    return true;
+  }
+  return false;
+};
+
 module.exports = {
   findUserByEmail,
+  checkPassword,
 };
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
-const { findUser } = require("../helpers");
+const { findUserByEmail, checkPassword } = require("../helpers");
 
 module.exports = (db) => {
   router.get("/", (req, res) => res.render("login"));
 
   router.post("/", (req, res) => {
-    console.log(req.body);
-  }); // router post
+    db.query(`SELECT * FROM users;`)
+      .then((data) => {
+        const result = {
+          authUser: false,
+          authPass: false,
+        };
+        const users = data.rows;
+        const findUser = findUserByEmail(req.body.email, users);
+        if (findUser) {
+          result.authUser = true;
+          const findPass = checkPassword(
+            findUser,
+            req.body.email,
+            req.body.password
+          );
+          if (findPass) {
+            req.session.user_id = findUser.id;
+            result.authPass = true;
+            res.redirect("/");
+          }
+        }
+        res.json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
   return router;
 };
