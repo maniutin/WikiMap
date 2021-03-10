778b56dd4011d5b5b1d7b8a0a2b801d1f7b3e5b0
Adam Marsala <adammarsala@hotmail.com>
2021-03-10 16:04:53 +0000

    Click event for reverse geocoding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 $(() => {
   console.log('App.js is ready');
+
 });
 
 
--- public/scripts/geocode.js
+++ public/scripts/geocode.js
@@ -1,3 +1,8 @@
 $(() => {
-  console.log("Geocode is ready");
+  setTimeout(() => {
+    const map = $('#map-viewport');
+    const mapID = $('#mapID').html();
+
+
+  }, 100);
 })
