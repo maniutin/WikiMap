2b2d5d12c9916b2b0c15c50d95ea15d4ad459520
Adam Marsala <adammarsala@hotmail.com>
2021-03-10 17:38:15 +0000

    Add favourite and contribute buttons to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,53 @@
 $(() => {
-  console.log('App.js is ready');
+  setTimeout(() => {
+    console.log('app.js is ready!');
 
-});
+    const map = $('#map-viewport');
+    const mapID = $('#mapID').html();
+    const favBtn = $('.favMap'); // Favourites button '<3'
+    const locBtn = $('.addLoc'); // button for 'Add Location'
+    const isUser = $('#userID').html(); // hidden p tag containing current user
+
+    favBtn.on('click', (e) => {
+      e.preventDefault();
+
+      $.ajax({
+        method: "POST",
+        url: "/",
+        data: { mapID: mapID }
+      })
+      .done(response => {
+        console.log(response);
+        //alert("Map added to favourites!");
+      })
+      .fail(err => console.log(err));
+
+      favBtn.blur();
+      alert("Map added to favourites!");
+    });
 
 
+    locBtn.on("click", function(event) {
+
+      const $addLocation = $('#addLocation');
+
+      if (isUser) {
+        console.log(isUser);
+        $addLocation.slideToggle({
+          duration: 400,
+          start: function() {
+            $(this).css('display', 'flex');
+          }
+        });
+        $('#location').focus();
+      } else {
+        alert("You must be a registered user to contribute to a map!");
+      }
+
+    });
+
+
+
+
+  }, 100);
+})
--- public/scripts/geocode.js
+++ public/scripts/geocode.js
@@ -1,8 +1,17 @@
 $(() => {
   setTimeout(() => {
+    // Set hover tooltip over favourites link
+    // const favBtn = $('[data-toggle="tooltip"]')
+    // favBtn.tooltip();
+
+    $('#favMap').on('click', (e) => {
+      e.preventDefault();
+      console.log('FAVOURITED');
+    });
+
     const map = $('#map-viewport');
     const mapID = $('#mapID').html();
 
 
-  }, 100);
+  }, 0);
 })
