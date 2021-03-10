e0dc391e919bfa9704cbec7647e6237cefdc4e99
Adam Marsala <adammarsala@hotmail.com>
2021-03-10 03:53:02 +0000

    Add map point to map successfully using form submission

--- routes/maps.js
+++ routes/maps.js
@@ -5,11 +5,14 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const dbParams = require("./../lib/db.js");
+const dbParams = require("./../lib/db");
 const { use } = require("bcrypt/promises");
 const { user } = require("pg/lib/defaults");
-const { getMapPoints } = require('./../lib/getMapPoints.js');
+const { getMapPoints } = require('./../lib/getMapPoints');
+const { getCoords } = require('../lib/getCoords');
+const axios = require('axios');
 const express = require('express');
+const { response } = require("express");
 const router  = express.Router();
 
 module.exports = (db) => {
@@ -108,16 +111,17 @@ module.exports = (db) => {
     const queryUserID = [userID];
 
     Promise.all([
-      Promise.resolve(
-        db.query(`SELECT * FROM maps WHERE id = $1;`, queryParams)
-      ),
+      Promise.resolve(db.query(`SELECT * FROM maps WHERE id = $1;`, queryParams)),
       Promise.resolve(db.query(`SELECT * FROM users WHERE id = $1;`, queryUserID)),
+      getMapPoints(db, queryParams)
     ])
       .then((all) => {
         const map = all[0].rows[0];
         const user = all[1].rows;
+        const points = all[2].rows;
         let templateVars = {
           map: map,
+          points: points,
           key: dbParams.api,
           user: userID ? user[0].email : null,
         };
@@ -133,6 +137,7 @@ module.exports = (db) => {
       });
   });
 
+  // This route is for the ajax call from initMap.js when the Google Maps API is requested
   router.get("/:mapID/start_coordinates", (req, res) => {
 
     const queryParams = [req.params.mapID];
@@ -153,6 +158,46 @@ module.exports = (db) => {
         .json({ error: err.message });
     });
   });
+
+    // This route is for the ajax call from initMap.js when the Google Maps API is requested
+    router.post("/:mapID/edit", (req, res) => {
+
+      const userID = req.session.user_id ? req.session.user_id : 0;
+
+      if (!userID) {
+        return res.redirect("/");
+      }
+      //?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${dbParams.api}`;
+      const data = req.body;
+      const address = req.body.address;
+
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json`, {
+        params: {
+          address: address,
+          key: dbParams.api
+        }
+      })
+        .then(response => {
+          const coords = response.data.results[0].geometry.location;
+          const queryParams = [userID, req.params.mapID, coords.lat, coords.lng, data.markerTitle, data.markerDesc];
+          const queryString = `
+          INSERT INTO map_points (user_id, map_id, latitude, longitude, title, description)
+          VALUES ($1, $2, $3, $4, $5, $6);`;
+          // Insert new map marker into db
+          db.query(queryString, queryParams)
+            .then((insert) => {
+              console.log(insert.rows);
+              res.redirect(`/maps/${req.params.mapID}`);
+            })
+            .catch((err) => console.error("query insert error:", err));
+        })
+        .catch(err => {
+          console.log('Geocode error: ', err);
+        });
+
+
+
+    });
 
   return router;
 };
