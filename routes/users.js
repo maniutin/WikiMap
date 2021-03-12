1672231d279974595dce44a577a9a5d47e1c8532
Andrew <andrew.ku93@gmail.com>
2021-03-12 01:14:37 +0000

    remove favourites from nav

--- routes/users.js
+++ routes/users.js
@@ -53,29 +53,29 @@ module.exports = (db) => {
   });
 
   // Can remove this route, its in the above route
-  router.get("/:userId/favourites", (req, res) => {
-    const currentUser = req.session.user_id;
-    // const currentUser = req.params.userId;
-    console.log(currentUser);
-    const templateVars = {
-      user: currentUser,
-    };
+  // router.get("/:userId/favourites", (req, res) => {
+  //   const currentUser = req.session.user_id;
+  //   // const currentUser = req.params.userId;
+  //   console.log(currentUser);
+  //   const templateVars = {
+  //     user: currentUser,
+  //   };
 
-    const queryString = `SELECT favourites.*, maps.*, users.name FROM favourites JOIN maps ON map_id = maps.id JOIN users on users.id = user_id WHERE user_id = $1;`;
-    const queryParams = [currentUser];
+  //   const queryString = `SELECT favourites.*, maps.*, users.name FROM favourites JOIN maps ON map_id = maps.id JOIN users on users.id = user_id WHERE user_id = $1;`;
+  //   const queryParams = [currentUser];
 
-    db.query(queryString, queryParams)
-      .then((favouritesResponse) => {
-        templateVars.favourites = favouritesResponse.rows;
-        templateVars.user = favouritesResponse.rows[0].name;
+  //   db.query(queryString, queryParams)
+  //     .then((favouritesResponse) => {
+  //       templateVars.favourites = favouritesResponse.rows;
+  //       templateVars.user = favouritesResponse.rows[0].name;
 
-        console.log(templateVars);
-        res.render("favourites", templateVars);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
+  //       console.log(templateVars);
+  //       res.render("favourites", templateVars);
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ error: err.message });
+  //     });
+  // });
 
   router.post("/:userId/favourites", (req, res) => {
     console.log("got the post req!");
